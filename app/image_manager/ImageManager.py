707dc211570c7198c09aaba9d10173b22be12695
wtlgo <georgeendo0@gmail.com>
2024-04-27 12:10:39 +0300

    Refactor ImageManager.py

--- app/image_manager/ImageManager.py
+++ app/image_manager/ImageManager.py
@@ -1,26 +1,15 @@
 import os
 
-from cryptography.fernet import Fernet
 
+from app.image_manager.Crypto import Crypto
 from app.image_manager.create_db import create_db
 
 
 class ImageManager:
     def __init__(self, db_name="image_database.db", key_file="key.key"):
-        self.images = create_db(db_name).image
-        self.db_name = db_name
-        self.key = self.load_or_create_key(key_file)
-
-    @staticmethod
-    def load_or_create_key(key_file):
-        if os.path.exists(key_file):
-            with open(key_file, "rb") as f:
-                return f.read()
-        else:
-            key = Fernet.generate_key()
-            with open(key_file, "wb") as f:
-                f.write(key)
-            return key
+        self._images = create_db(db_name).image
+        self._db_name = db_name
+        self._crypto = Crypto(key_file)
 
     def add_image(self, image_path, image_hash):
         if not os.path.isfile(image_path):
@@ -31,8 +20,9 @@ class ImageManager:
             image_bytes = f.read()
 
             (
-                self.images.insert(
-                    image_hash=image_hash, image_data=self.encrypt_data(image_bytes)
+                self._images.insert(
+                    image_hash=image_hash,
+                    image_data=self._crypto.encrypt_data(image_bytes),
                 )
                 .on_conflict_replace()
                 .execute()
@@ -42,29 +32,21 @@ class ImageManager:
 
     def get_image_data(self, image_hash):
         try:
-            img = self.images.get(self.images.image_hash == image_hash)
+            img = self._images.get(self._images.image_hash == image_hash)
             if img:
-                return self.decrypt_data(img.image_data)
+                return self._crypto.decrypt_data(img.image_data)
         except Exception as _:
             print(f"Ошибка: Изображение не найдено в базе данных.")
             return None
 
     def get_image_hashes(self):
-        return [res.image_hash for res in self.images.select()]
+        return [res.image_hash for res in self._images.select()]
 
     def remove_image(self, image_hash):
-        self.images.delete().where(self.images.image_hash == image_hash).execute()
+        self._images.delete().where(self._images.image_hash == image_hash).execute()
         print(f"Изображение удалено из базы данных.")
 
-    def encrypt_data(self, data):
-        cipher = Fernet(self.key)
-        return cipher.encrypt(data)
-
-    def decrypt_data(self, encrypted_data):
-        cipher = Fernet(self.key)
-        return cipher.decrypt(encrypted_data)
-
     def backup_database(self, backup_file):
-        with open(self.db_name, "rb") as f_src:
+        with open(self._db_name, "rb") as f_src:
             with open(backup_file, "wb") as f_dest:
                 f_dest.write(f_src.read())
