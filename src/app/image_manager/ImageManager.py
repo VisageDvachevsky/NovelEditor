9f01a3ad4afb666e39e38e6635b542982117b3bd
wtlgo <georgeendo0@gmail.com>
2024-04-29 01:19:08 +0300

    Add ImageManager_test.py

--- src/app/image_manager/ImageManager.py
+++ src/app/image_manager/ImageManager.py
@@ -6,25 +6,22 @@ from app.crypto_fs.load_root import load_root
 
 
 class ImageManager:
-    def __init__(self, key_file="key.key"):
-        self._fs = CryptoFs("data", load_root(key_file))
+    def __init__(self, root_path: str = "data", key_file="key.key"):
+        self._fs = CryptoFs(root_path, load_root(key_file))
 
     def add_image(self, image_path, image_hash):
         path = Path(image_path)
         if not path.exists() or not path.is_file():
             logger.error(f"Файл изображения '{image_path}' не найден.")
-            return
+            return False
 
         image_bytes = path.read_bytes()
         self._fs.write_file(["images", image_hash], image_bytes)
         logger.info(f"Изображение добавлено в базу данных.")
+        return True
 
     def get_image_data(self, image_hash):
-        try:
-            return self._fs.read_file(["images", image_hash])
-        except Exception as _:
-            logger.error(f"Изображение не найдено в базе данных.")
-            return None
+        return self._fs.read_file(["images", image_hash])
 
     def remove_image(self, image_hash):
         self._fs.remove(["images", image_hash])
