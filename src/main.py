b94754c7badf3163785a27ac27983785c862511e
wtlgo <georgeendo0@gmail.com>
2024-04-30 17:42:33 +0300

    Add flet template

--- src/main.py
+++ src/main.py
@@ -1,17 +1,8 @@
-import sys
-
-from loguru import logger
-
-from app.visual import run
+import flet as ft
 
 
-def main(args: list[str]) -> None:
-    if len(args) < 2:
-        logger.error("Не хватает параметров!")
-        return
-
-    run(args[1])
+def main(page: ft.Page):
+    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
 
 
-if __name__ == "__main__":  # pragma: no cover
-    main(sys.argv)
+ft.app(main)
